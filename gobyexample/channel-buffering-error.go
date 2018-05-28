a4061dbb6cc2e5c33f49bbafc635236c530a4322
Jose David Baena <josedab@gmail.com>
2018-05-28 14:16:22 -0700

    Learning Go: Go By Example: Channels - Channels - Channel buffering - Channel directions - Channel synchronization - Select

---
+++ gobyexample/channel-buffering-error.go
@@ -0,0 +1,32 @@
+// By default channels are _unbuffered_, meaning that they
+// will only accept sends (`chan <-`) if there is a
+// corresponding receive (`<- chan`) ready to receive the
+// sent value. _Buffered channels_ accept a limited
+// number of  values without a corresponding receiver for
+// those values.
+
+package main
+
+import "fmt"
+
+func main() {
+
+	// Here we `make` a channel of strings buffering up to
+	// 2 values.
+	messages := make(chan string, 2)
+
+	// Because this channel is buffered, we can send these
+	// values into the channel without a corresponding
+	// concurrent receive.
+	messages <- "buffered"
+	messages <- "channel"
+
+	// Error that should happen is:
+	// all goroutines are asleep - deadlock!
+	// This is due to the channels acting like a blocking queue
+	messages <- "this should cause an error"
+
+	// Later we can receive these two values as usual.
+	fmt.Println(<-messages)
+	fmt.Println(<-messages)
+}
